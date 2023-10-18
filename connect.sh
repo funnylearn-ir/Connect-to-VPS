c5a2a35f46e4115786f1a57c0a150a029542955d
funnylearn.ir | فانی لرن <121741285+funnylearn-ir@users.noreply.github.com>
2023-10-18 08:16:45 +0330

    Create connect.sh

---
+++ connect.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p $'\033[1;32mEnter the \033[1mIP address\033[0m\033[1;32m of the server: \033[0m' IP
+
+read -p $'\033[1;32mEnter the \033[1mport\033[0m\033[1;32m of the server (Press Enter for default port 22): \033[0m' PORT
+
+if [ -z "$PORT" ]; then
+    PORT=22
+fi
+
+echo "Connecting to the server..."
+
+ssh user@$IP -p $PORT
